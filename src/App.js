f5e929919f7177abe4d4e9d1d9a040291fadb16c
Aderson <deco.neto10@gmail.com>
2021-07-10 20:33:16 -0300

    finish form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
+import "fontsource-roboto";
+//import { validarCPF, validarSenha } from "./modelo/cadastro";
+import ValidacoesCadastro from "./contexts/validacoesCadastro";
+import { validarCPF, validarSenha } from "./modelo/cadastro";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Fomulario de cadastro</h1>
-        <FormularioCadastro />
-      </div>
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" align="center" component="h1">
+          Fomulario de cadastro
+        </Typography>
+        <ValidacoesCadastro.Provider
+          value={{ cpf: validarCPF, senha: validarSenha, nome: validarSenha }}
+        >
+          <FormularioCadastro aoEnviar={aoEnviarform} />
+        </ValidacoesCadastro.Provider>
+      </Container>
     );
   }
+}
+
+function aoEnviarform(dados) {
+  console.log(dados);
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
+import { useEffect } from "react";
+import { Typography, Stepper, Step, StepLabel } from "@material-ui/core";
 
-function FormularioCadastro() {
+function FormularioCadastro({ aoEnviar }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});
+
+  useEffect(() => {
+    if (etapaAtual === formulario.length - 1) {
+      aoEnviar(dadosColetados);
+    }
+  });
+
+  const formulario = [
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variant="h5"> Obrigado pelo cadastro!</Typography>,
+  ];
+
+  function coletarDados(dados) {
+    setDados({ ...dadosColetados, ...dados });
+    proximo();
+  }
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+
   return (
-    <form>
-      <input type="text" />
-    </form>
+    <>
+      <Stepper activeStep={etapaAtual + 1}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {formulario[etapaAtual]}
+    </>
   );
 }
 
